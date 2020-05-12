ecdacf7d6b0e24012953cb5e85e4fad1de877e2c
kiranalda <kiranalda@gmail.com>
2020-05-12 15:49:27 +0530

    Added the csv part

--- BooksToscrape/scraper.py
+++ BooksToscrape/scraper.py
@@ -1,5 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
+
+csv_file = open('scrape.csv', 'w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Title', 'Price', 'Stock Availability'])
 
 source = requests.get('http://books.toscrape.com/').text
 
@@ -15,11 +20,14 @@ for article in articles:
     #print(article.prettify)
     print('----------------------------')
     print('TITLE::' + article.h3.text)
+    title = article.h3.text
     try:
         product = article.find('div' , class_='product_price')
         price = product.find('p' , class_='price_color')
         stock = product.find('p' , class_='instock availability')
         try:
+            prc = price.text
+            stck = stock.text.strip()
             print('PRICE::' + price.text)
             print('STOCK AVAILABILITY:: ' + stock.text.strip())
         except:
@@ -27,6 +35,8 @@ for article in articles:
             stock = None
     except:
         product = None
+		
+    csv_writer.writerow([title,prc,stck])
     
         
 	
